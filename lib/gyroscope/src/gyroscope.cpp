dc365e1e7951c908a2352cb78343033d0d41608c
thilak <tilaksai007@gmail.com>
2021-11-20 12:08:46 +0530

    gyro library working

---
+++ lib/gyroscope/src/gyroscope.cpp
@@ -0,0 +1,33 @@
+#include "gyroscope.h"
+
+uint8_t gyroscope::init(uint8_t address)
+{
+    this->_address = address;
+    uint8_t data = 0x0F;
+    // DR = 100, BW = 32 enable all axes
+    uint8_t retVal = sensor_writeRegister(address, CTRL_REG1, 1, &data);
+    return retVal;
+}
+
+uint8_t gyroscope::readData(int16_t& x, int16_t& y, int16_t& z)
+{
+    uint8_t data[6];
+    uint8_t status;
+    do
+    {
+        sensor_readRegister(this->_address, STATUS_REG, 1, &status);
+    } while (((status >> 3) & 0x01) == 0);
+
+    uint8_t retVal = sensor_readRegister(this->_address, OUT_X_L | READ, 6, data);
+    x = ((data[0] | (data[1] << 8)));
+    y = ((data[2] | (data[3] << 8)));
+    z = ((data[4] | (data[5] << 8)));
+    return retVal;
+}
+
+uint8_t gyroscope::found()
+{
+    uint8_t data;
+    sensor_readRegister(this->_address, WHO_AM_I | READ, 1, &data);
+    return (data==0xD3);
+}
---
+++ lib/gyroscope/src/gyroscope.h
@@ -0,0 +1,25 @@
+#ifndef GYROSCOPE_H
+#define GYROSCOPE_H
+
+#include <Arduino.h>
+#include <sensor.h>
+
+#define WHO_AM_I  0x0F
+#define CTRL_REG1 0x20
+#define STATUS_REG 0x27
+#define OUT_X_L 0x28
+
+#define READ (1 << 7)
+
+class gyroscope
+{
+private:
+    /* data */
+    uint8_t _address;
+public:
+    uint8_t init(uint8_t address);
+    uint8_t readData(int16_t& x, int16_t& y, int16_t& z);
+    uint8_t found();
+};
+
+#endif
